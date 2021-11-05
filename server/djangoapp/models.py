e4af7706cbca051d4e2e6ce0543a951712a892df
elmachotroso <me@andreivictor.net>
2021-11-06 00:42:43 +0800

    Fixed models and added admin models.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.utils.timezone import now
 
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
 class CarMake(models.Model):
     name = models.CharField(max_length=256)
     description = models.CharField(max_length=512)
@@ -17,15 +9,6 @@ class CarMake(models.Model):
     def __str__(self):
         return f"CarMake name={self.name} description={self.description}"
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
 class CarModel(models.Model):
     TYPE_SEDAN = "Sedan"
     TYPE_SUV = "SUV"
@@ -36,13 +19,13 @@ class CarModel(models.Model):
         (TYPE_SUV, "SUV"),
         (TYPE_WAGON, "WAGON"),
         (TYPE_ETC, "Others"))
-    modelId = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    makeId = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     dealerId = models.IntegerField(null=False)
     type = models.CharField(max_length=128, choices=TYPE_CHOICES, default=TYPE_SEDAN)
     year = models.DateField()
     def __str__(self):
-        return f"modelId={modelId} name={name} dealerId={dealerId} type={type} year={year}"
+        return f"makeId={self.makeId} name={self.name} dealerId={self.dealerId} type={self.type} year={self.year}"
 
 
 
